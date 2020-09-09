6f0f8a3417dcd171f32623d76c22ae6742d0aa92
Rahim Ahmed <rahim.ahmed@learnet.se>
2020-09-09 14:57:27 +0200

    Övat på variabler, konkantinerat

---
+++ Kapitel 2/Exempel-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exempel_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklarera en variable för namnet
+            string namn = "";
+            string hobby = "";
+            string meddelande = "";
+
+            // Läs in namnet
+            Console.Write("Vad heter du? ");
+            namn = Console.ReadLine();
+
+            // Läs in hobby
+            Console.Write("Vad gillar du att göra? ");
+            hobby = Console.ReadLine();
+
+            // sätter samman ett meddelande
+            meddelande = "Hej! " + namn + ", du gillar att " + hobby + ". ";
+
+
+            // Skriver ut meddelandet
+            Console.WriteLine(meddelande);
+        }
+    }
+}
---
+++ kapitel 1/Konsolen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Konsolen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ändra bakgrundsfärg på terminalen
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            //Ändra textfärg i terminalen
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
