d7b65f6a13b93cb10ecae260813305dfe4f102ab
Rahim Ahmed <rahim.ahmed@learnet.se>
2020-09-09 13:42:52 +0200

    tränat på redigering

---
+++ kapitel 1/Uppgift 1-2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+Skapa ett nytt program som skriver ut följande text:
+*/
+
+using System;
+
+namespace Uppgift_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Denna skriver ut text
+            /*Console.WriteLine("Detta är ett program gjort i C#.");
+            Console.WriteLine("Programmet skriver ut text på flera rader.");
+            Console.WriteLine("Detta är programmets sista utskrift.");*/
+
+           Console.WriteLine("Detta är ett \"program\" gjort i C#.\n" +
+           "\\Programmet skriver ut text på flera rader.\n" +
+           "Detta\tär\tprogrammets\tsista\tutskrift.");
+
+
+        }
+    }
+}
