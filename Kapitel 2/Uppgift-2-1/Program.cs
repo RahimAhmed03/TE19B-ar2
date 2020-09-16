6433045c8b99f0b5a4f6024310af08cdf70a65dc
Rahim Ahmed <rahim.ahmed@learnet.se>
2020-09-16 14:57:07 +0200

    Övat på variabler och inmatning

---
+++ Kapitel 2/Uppgift-2-1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Skapa ett program som frågar användaren vilket hur högt hen kan hoppa i höjdhopp mätt i meter. Berätta för användaren hur mycket högre världsrekordet i höjdhopp är (världsrekordet i höjdhopp är 2,45 meter).
+
+
+using System;
+
+namespace Uppgift_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur högt kan du hoppa i höjdhopp i meter?");
+            string höjdText = Console.ReadLine();
+            double höjd = double.Parse(höjdText);
+            double skillnad = 2.45 - höjd;
+
+            Console.WriteLine("Världsrekordet är " + skillnad + " meter");
+        }
+    }
+}
---
+++ Kapitel 2/Uppgift-2-4/Program.cs
@@ -0,0 +1,47 @@
+﻿
+/* 
+Skapa ett program som frågar användaren efter två tal. Programmet ska sedan beräkna och skriva ut summan av talen och därefter produkten av talen.
+ */
+
+using System;
+
+namespace Uppgift_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Läs in första argumentet
+            int tal1 = int.Parse (args[0]);
+            // Läs in andra argument
+            int tal2 = int.Parse (args[1]);
+
+
+            // Räkna ut summan och skriv ut
+            int summa = tal1 + tal2;
+            Console.WriteLine("Summan = " + summa);
+
+            // Räkna ut produkten ich skriv ut
+            int produkt = tal1 * tal2;
+            Console.WriteLine("Produkten = " + produkt);
+
+
+
+/*             // Läs in ett heltal
+            Console.WriteLine("Ange tal1: ");
+            int tal1 = int.Parse(Console.ReadLine());
+
+            // Läs in ett heltall till
+            Console.Write("Ange tal2: ");
+            int tal2 = int.Parse(Console.ReadLine());
+
+            // Räkna ut summan och skriv ut
+            int summa = tal1 + tal2;
+            Console.WriteLine("Summan = " + summa);
+
+            // Räkna ut produkten ich skriv ut
+            int produkt = tal1 * tal2;
+            Console.WriteLine("Produkten = " + produkt); */
+        }
+    }
+}
