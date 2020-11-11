262e0cfc5a2ee1ddb22ebd3821583a78406bc5f1
Rahim Ahmed <rahim.ahmed@learnet.se>
2020-11-11 14:56:55 +0100

    lyckohjul

---
+++ Kapitel 4/Lyckohjul/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lyckohjul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Läs in en gissning
+            Console.Write("Gissa ett tal (1-10)? ");
+            int gissning = int.Parse(Console.ReadLine());
+
+            Random slumptalsgenerator = new Random();
+            int slumptal2 = slumptalsgenerator.Next(1, 11);
+
+            // vad slumptalet blir
+            Console.WriteLine("slumptalet är " + slumptal2);
+
+            // Kolla om gissningen är korrekt
+            if (gissning == slumptal2)
+            {
+                Console.WriteLine("Rätt svar!");
+            }
+            else
+            {
+                Console.WriteLine("Fel svar!");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Kapitel 4/TarningSimulator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TarningSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fråga hur många gånger tärningen skall du kasta?
+            Console.WriteLine("Hur många gånger vill du kasta tärningen? ");
+            int tärning = int.Parse(Console.ReadLine());
+
+            // Fråga hur många sidor har tärningen?
+            Console.WriteLine("Hur många sidor har tärningen? ");
+            int sidor = int.Parse(Console.ReadLine());
+
+            Random slumptalsgenerator = new Random();
+            
+
+            // Kasta tärningen ett antal gånger
+            // För varje kast kan man ett slumptal med så många sidor
+            for (int i = 0; i < tärning; i++)
+            {
+                int slumptal2 = slumptalsgenerator.Next(1, sidor + 1);
+                Console.WriteLine(slumptal2);
+            }
+            // Skriv ut varje kast
+        }
+    }
+}
