d09f501d512a706ea46967e7251d2aa085a13f88
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2021-01-14 11:09:43 +0100

    Träna på filhantering

---
+++ Labbar/Filhantering2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Filhantering2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Filnamnet
+            string filnamn = "./animals.txt";
+
+            // Läs in alla rader
+            string[] rader = File.ReadAllLines(filnamn);
+
+            // Läs in sökterm
+            Console.Write("Ange sökterm (första bokstaven): ");
+            string sökterm = Console.ReadLine();
+
+            // Loopa igenom arrayen
+            // Dvs gå igenom rad-för-rad
+            int räknare = 1;
+            foreach (var rad in rader)
+            {
+                // Kolla om första bokstaven = sökterm
+                if (rad.Substring(0, 1) == sökterm)
+                {
+                    Console.WriteLine(räknare + " " + rad);
+                    räknare++;
+                }
+
+                
+            }
+
+            // Antal hittade rader
+            Console.WriteLine($"Enligt mina beräkningar finns det {räknare - 1} djur");
+        }
+    }
+}
