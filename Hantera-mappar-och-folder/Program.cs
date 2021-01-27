38af53f9315b9ce69b0be6fe153a2b5dbdd83d27
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2021-01-27 15:01:44 +0100

    Hitta land uppgift tränade på debugging

---
+++ Hantera-mappar-och-folder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hantera_mappar_och_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // skapa en väg för att lokalisera filsystem
+            string rootPath = @"C:\Temp\Demos\FileSystem";
+
+            // Läser in allt i katalogen
+            string[] dirs = Directory.GetDirectories(rootPath)
+
+            foreach (string dir in dirs
+            {
+                
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Labbar/Countries/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Countries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // presentera programmet
+            Console.WriteLine("Program för att hitta länder matchade sökterm.");
+            Console.WriteLine("Program listar land och landskod.");
+
+            // Läser in alla rader i textfilen, om det finns
+            if (File.Exists("countries.csv"))
+            {
+                // Läser in alla rader
+                string[] rader = File.ReadAllLines("countries.csv");
+
+                // Ber användaren om sökterm
+                Console.Write("Ange sökterm (avsluta med return): ");
+                string sökterm = Console.ReadLine();
+
+                // Loopa igenom alla rader
+                foreach (var rad in rader)
+                {
+                    // Söka i den röda fältet
+                    //Console.WriteLine(rad);   // rad är en string
+                    string[] delar = rad.Split(',');
+                    
+                }   
+            } else
+            {
+                Console.WriteLine("Avbryter! Filen countries.cvs finns inte.");
+            }
+        }
+    }
+}
