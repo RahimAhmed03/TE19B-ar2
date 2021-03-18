659b3d6a2169777fb046f5ac1df075183f97728e
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2021-03-18 10:17:46 +0100

    metodkommentarer, Readint, tryparse

---
+++ Kapitel-6/Uppgift-6-10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uppgift_6_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Be användaren att skriva in något
+            Console.WriteLine("Programmet ska förstå ifall det du skrev är ett heltal eller inte. Var vänligen skriv något");
+            TestInt();
+        }
+
+        /// <summary>
+        /// Metod för att programmet ska förså ifall det är ett tal eller inte
+        /// </summary>
+        static void TestInt()
+        {
+            int tal = 0;
+            if (int.TryParse(Console.ReadLine(), out tal))
+            {
+                // Ifall det är ett tal skriv "det är ett tal"
+                Console.WriteLine("Det var ett tal. Tack för det");
+            }
+            else
+            {
+                // Ifall det inte är ett tal skriv "det är inte ett tal
+                Console.WriteLine("Ditt svar var inte ett tal.");
+            }
+        }
+    }
+}
---
+++ Kapitel-6/Uppgift-6-9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift_6_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Be om ett tal
+            Console.WriteLine("Skriv ett tal");
+            double tal = ReadDouble();
+            Console.WriteLine($"Du skrev talet {tal} ");
+        }
+
+        // Metod för att föra fram decimaltal
+        static double ReadDouble()
+        {
+            double tal;
+            while (double.TryParse(Console.ReadLine(), out tal) == false)
+            {
+                // Ifall det inte är ett giltigt tal skriv felmeddelande
+                Console.WriteLine("Du skrev inte ett tal, vg försök igen");   
+            }
+            // Returnera talet
+            return tal;
+        }
+    }
+}
