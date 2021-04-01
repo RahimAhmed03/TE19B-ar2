6e4b1d290723ec5f9393d892782eb700ad39d51a
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2021-04-01 10:14:15 +0200

    Överlagra metoder och valbara argument

---
+++ Kapitel-6/Exempel-7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exempel_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ange en text att kryptera:");
+            string meddelande = Console.ReadLine();
+
+            Console.WriteLine($"Krypterat med 1 steg:{CeasarKryptera(meddelande)}");
+            Console.WriteLine($"Krypterat med 3 steg:{CeasarKryptera(meddelande, 3)}");
+        }
+
+        static string CeasarKryptera(string text, int nyckel = 1)
+        {
+            // Loopa igenom texten, tecken för tecken
+            for (int i = 0; i < text.Length; i++)
+            {
+                // Plocka ut ett tecken
+                char tecken = text[i];
+
+                // Ta reda teckenkoden
+                int teckenKod = (int)tecken;
+
+                // Räkna ut nu krypterar tecken
+                int teckenKodKrypterat = teckenKod + nyckel;
+
+                char teckenKrypterat = (char)teckenKodKrypterat;
+            }
+
+            return..;
+        }
+    }
+}
---
+++ Kapitel-6/Uppgift-6-15/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Uppgift_6_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SkrivBaklänges("European Space Agency");
+
+            SkrivBaklänges(1975);
+
+            SkrivBaklänges(6.7);
+        }
+
+        /// <summary>
+        /// Skriver meddelandet baklänges i en rad
+        /// </summary>
+        /// <param name="meddelande"></param>
+        static void SkrivBaklänges(string meddelande)
+        {
+            for (int i = meddelande.Length - 1; i >= 0; i--)
+            {
+                Console.Write(meddelande[i]);
+            }
+        }
+
+        /// <summary>
+        /// Skriver talet baklänges i en rad
+        /// </summary>
+        /// <param name="tal"></param>
+        static void SkrivBaklänges(int tal)
+        {
+            SkrivBaklänges(tal.ToString());
+        }
+
+        /// <summary>
+        /// Skriver tal med komma/punkt i en rad
+        /// </summary>
+        /// <param name="talet"></param>
+        static void SkrivBaklänges(double talet)
+        {
+            SkrivBaklänges(talet.ToString());
+        }
+    }
+}
